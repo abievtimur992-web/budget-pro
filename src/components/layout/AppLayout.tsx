b82103c5736f44438e7cc03b9b37879867900502
abievtimur992-web <abievtimur992@gmail.com>
2026-08-28 23:40:25 +0500

    Fix flex container horizontal scroll overflow

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -199,7 +199,7 @@ export const AppLayout = () => {
             <LanguageSelector />
           </div>
         </header>
-        <div className="flex-1 overflow-auto p-4 md:p-8">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden p-4 md:p-8">
           <Outlet />
         </div>
       </main>
